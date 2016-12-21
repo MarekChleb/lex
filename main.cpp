5b8c0f3ee6ecc4560404cc6b9d329ed01ef13276
Marek <mw332637@students.mimuw.edu.pl>
2016-12-21 18:52:59 +0100

    do the rest

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <string>
 
 #define MAX_NM 300006
 #define BIGGER >
@@ -8,12 +10,17 @@
 
 bool DEBUG = true;
 int N, M;
+string word;
+map<string> sufixes;
 
 using namespace std;
 
 void read_input() {
     scanf("%d%d", &N, &M);
-
+    cin >> word;
+    for (int i = 1; i <= N; i++) {
+        sufixes.insert(word.substr(N - 1, i));
+    }
 }
 
 int main() {
