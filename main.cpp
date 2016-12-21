905b5e2b92049651bc424916b232a48f206f58d5
Marek <mw332637@students.mimuw.edu.pl>
2016-12-21 13:37:49 +0100

    small tweaks

--- main.cpp
+++ main.cpp
@@ -2,10 +2,21 @@
 #include <vector>
 
 #define MAX_NM 300006
+#define BIGGER >
+#define SMALLER <
+#define EQUAL =
+
+bool DEBUG = true;
+int N, M;
 
 using namespace std;
 
-int main() {
+void read_input() {
+    scanf("%d%d", &N, &M);
 
+}
+
+int main() {
+    read_input();
     return 0;
 }
